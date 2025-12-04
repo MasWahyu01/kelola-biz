65961b2cebbb1540ac83d0a34a4721735c87bbd7
MasWahyu01 <blue.edtz1@gmail.com>
2025-12-04 20:27:40 +0700

    Perbaikan Global Bootstrap (Fix Loading Stuck)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,9 @@
 import './bootstrap';
-import 'bootstrap'; 
 
-// TAMBAHKAN INI: Import CSS Ikon via JS agar file font otomatis ter-copy
+// --- PERUBAHAN DI SINI ---
+// Kita import semua fitur Bootstrap dan simpan ke variabel global 'window'
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
+// -------------------------
+
 import 'bootstrap-icons/font/bootstrap-icons.css';
